ef7ea585059bc0ca48053430720761c1c2e67579
Luis Hernandez <manson.louis@gmail.com>
2022-03-26 03:11:01 -0700

    fix identation from 4 tabs to 2 spaces

--- btn_funcion.js
+++ btn_funcion.js
@@ -1,13 +1,12 @@
-function loginf()
-{	
+function loginf() {	
 	document.getElementById("register_form").style.display = "none";
 	document.getElementById("login_form").style.display = "block";
 	
 	document.getElementById("button_login").className = "btn btn-success";
 	document.getElementById("button_register").className = "btn btn-secondary";
 }
-function regf()
-{	
+
+function regf() {	
 	document.getElementById("register_form").style.display = "block";
 	document.getElementById("login_form").style.display = "none";
 
