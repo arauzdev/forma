93a8bfd1be38600b8726b6e6e7037cc616632a40
arauzdev <44212037+arauzdev@users.noreply.github.com>
2022-03-27 21:26:32 -0700

    Update btn_funcion.js
    Fixed indentation from tabs to spaces.

--- btn_funcion.js
+++ btn_funcion.js
@@ -1,16 +1,16 @@
 function loginf() {	
-	document.getElementById("register_form").style.display = "none";
-	document.getElementById("login_form").style.display = "block";
-	
-	document.getElementById("button_login").className = "btn btn-success";
-	document.getElementById("button_register").className = "btn btn-secondary";
+  document.getElementById("register_form").style.display = "none";
+  document.getElementById("login_form").style.display = "block";
+
+  document.getElementById("button_login").className = "btn btn-success";
+  document.getElementById("button_register").className = "btn btn-secondary";
 }
 
 function regf() {	
-	document.getElementById("register_form").style.display = "block";
-	document.getElementById("login_form").style.display = "none";
+  document.getElementById("register_form").style.display = "block";
+  document.getElementById("login_form").style.display = "none";
 
 
-	document.getElementById("button_login").className = "btn btn-secondary";
-	document.getElementById("button_register").className = "btn btn-success";
+  document.getElementById("button_login").className = "btn btn-secondary";
+  document.getElementById("button_register").className = "btn btn-success";
 }
